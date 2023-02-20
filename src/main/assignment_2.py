48c03399332ccf8a945dc308200e59c677e67ce8
Jonathan Washuta <122390678+jwashuta@users.noreply.github.com>
2023-02-20 11:52:17 -0500

    Update assignment_2.py

--- src/main/assignment_2.py
+++ src/main/assignment_2.py
@@ -1,35 +1,21 @@
 import numpy as np
-from scipy.linalg import solve
-# 1. Using Neville's method, find the 2nd degree interpolating value for f(3.7) for the 
-#    following set of data: x - 3.6, 3.8, 3.9; f(x) - 1.675, 1.436, 1.318
-
+from scipy.linalg from solve
+# 1.
 def nevilles_method(x_points, y_points, approximated_x):
     size = len(x_points)
     matrix = np.zeros((size, size))
-
     for index, row in enumerate(matrix):
-        row[0] = y_points[index]
-    
+        row[0] = y_points[index]   
     num_of_points = len(x_points)
     for i in range(1, num_of_points):
         for j in range(1, i + 1):
             first_multiplication = (approximated_x - x_points[i - j]) * matrix[i][j - 1]
             second_multiplication = (approximated_x - x_points[i]) * matrix[i - 1][j - 1]
-
             denominator = x_points[i] - x_points[i - j]
-
             coefficient = (first_multiplication - second_multiplication) / denominator
-
             matrix[i][j] = coefficient
-    
-    # print(matrix, "\n") - check, comment out later
     print(matrix[num_of_points - 1][num_of_points - 1], "\n")
-
-# 2. Using Newton's forward method, print out the polynomial approximations for degrees 1, 2,
-#    and 3 using the following set of data: 
-#       (a) Hint, create the table first
-#       (b) x - 7.2, 7.4, 7.5, 7.6; f(x) - 23.5492, 25.3913, 26.8224, 27.4589
-
+# 2. 
 def newton_method_and_approx():
     x0 = 7.2
     x1 = 7.4
@@ -47,18 +33,12 @@ def newton_method_and_approx():
     third_dd = (second_dd_2 - second_dd_1) / (x3 - x0)
     d = [first_dd_1, second_dd_1, third_dd]
     print(d, "\n")
-    
-    # start of three
     approx_x = 7.3
     p_x = f_x0 + first_dd_1 * (approx_x - x0) + second_dd_1 * (approx_x - x1) * (approx_x - x0)\
           + third_dd * (approx_x - x2) * (approx_x - x1) * (approx_x - x0)
     print(p_x, "\n")
 
-# 4. Using the divided difference method, print out the Hermite polynomial approximation matrix
-#       x = 3.6, 3.8, 3.9
-#       f(x) = 1.675, 1.436, 1.318
-#       f'(x) = -1.195, -1.188, -1.182
-
+# 4. 
 def apply_div_diff(matrix: np.array):
     for i in range(2, len(matrix)):
         for j in range(2, i + 2):
@@ -68,10 +48,8 @@ def apply_div_diff(matrix: np.array):
 
             numerator = matrix[i][j - 1] - matrix[i - 1][j - 1]
             
-            # something get operation
             denominator = matrix[i][0] - matrix[i - j + 1][0]
 
-            # something save into matrix
             operation = numerator / denominator
             matrix[i][j] = operation
     return matrix
@@ -79,27 +57,22 @@ def apply_div_diff(matrix: np.array):
 def hermite_interpolation():
     x_points = [3.6, 3.8, 3.9]
     y_points = [1.675, 1.436, 1.318]
-
-    slopes = [-1.195, -1.188, -1.182]
-    
+    slopes = [-1.195, -1.188, -1.182] 
     size = len(x_points) 
     matrix = np.zeros((size * 2, size * 2))
 
-    # populate x values
     index = 0
     for x in range(0, size * 2, 2):
         matrix[x][0] = x_points[index]
         matrix[x + 1][0] = x_points[index]
         index += 1
         
-    # prepopulate
     index = 0
     for y in range(0, size * 2, 2):
         matrix[y][1] = y_points[index]
         matrix[y + 1][1] = y_points[index]
         index += 1
 
-    # prepopulate derivatives ( every other row )
     index = 0
     for x in range(1, size * 2 - 1, 2):
         matrix[x][2] = slopes[index]
@@ -107,16 +80,11 @@ def hermite_interpolation():
         matrix[5][2] = -1.182
         index += 1
 
-    # apply the divided differences
     filled_matrix = apply_div_diff(matrix)
 
     print(filled_matrix, "\n")
 
-# 5. Using cubic spline interpolation, solve for the following using this set of data:
-#    x - 2, 5, 8, 10; f(x) - 3, 5, 7, 9
-#       (a) Find matrix A
-#       (b) Vector b
-#       (c) Vector c       
+# 5.    
 
 def cubic_spline_matrix(x, y):
     size = len(x)
@@ -152,8 +120,6 @@ if __name__ == "__main__":
     x_points = [3.6, 3.8, 3.9]
     y_points = [1.675, 1.436, 1.318]
     approximated_x = 3.7 
-    #nevilles_method(x_points, y_points, approximated_x)
-
     # 2 and 3
     newton_method_and_approx()
 
